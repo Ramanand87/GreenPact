fbeaf2d60734c1d8764127a5ec0f079fc3acacca
k29laksh <klaxay0505@gmail.com>
2025-11-20 15:11:33 +0530

    ai filter added in  market and demand page

--- greenpact/user/serializers.py
+++ greenpact/user/serializers.py
@@ -43,6 +43,8 @@ class userSerializers(ModelSerializer):
             profile_data["signature"] = self.initial_data.get("signature")
             if not self.initial_data.get("qr_code_image"):
                 raise ValidationError({'error': 'Farmer QR code image is required'})
+            else :
+                profile_data["qr_code_image"] = self.initial_data.get("qr_code_image")
             models.FarmerProfile.objects.create(**profile_data)
         elif user.type == "contractor":
             profile_data["gstin"] = self.initial_data.get("gstin")
