2300c24d86f1b1e82b27bc9b7ac86fd0ee625171
Ramanand Kumawat <ram3036anand03@gmail.com>
2025-11-28 14:40:05 +0530

    Fix Docker setup and media files serving for Render

--- greenpact/greenpact/urls.py
+++ greenpact/greenpact/urls.py
@@ -3,6 +3,9 @@ from django.contrib import admin
 from django.urls import path,include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.static import serve
+from django.urls import re_path
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('user/',include('user.urls')),
@@ -12,7 +15,9 @@ urlpatterns = [
     path('chat/',include('chat.urls')),
     path('contracts/',include('contract.urls')),
     path('greenbot/',include('greenbot.urls')),
+    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+    re_path(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT}),
 ]
 
-urlpatterns+=static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)   
-urlpatterns+=static(settings.STATIC_URL,document_root=settings.STATIC_ROOT)   
+# urlpatterns+=static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)   
+# urlpatterns+=static(settings.STATIC_URL,document_root=settings.STATIC_ROOT)   
